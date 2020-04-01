abc7c439a909d1459d86d2b1110e75f5557fce2d
Tim Gentry <gentry.tim@gmail.com>
2020-04-01 14:26:05 -0400

    Added search bar component, event handlers

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    onSearchSubmit = (searchTerm) => {
+        console.log(searchTerm);
+    };
+
+    render() {
+        return <div className="ui container" style={{ marginTop: '10px' }}>
+            <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+    };
+};
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = {
+        searchTerm: ''
+    };
+
+    onInputChange = (e) => {
+        this.setState({ searchTerm: e.target.value });
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.searchTerm);
+    }
+
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" value={this.state.searchTerm} onChange={this.onInputChange} />
+                    </div>
+                </form>
+            </div>
+        );
+    };
+};
+
+export default SearchBar;
